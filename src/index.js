5b73d50c9ca2beac89b6c7c145db426ad626c8ed
Andy Menutti <andymenutti@gmail.com>
2022-07-19 16:45:42 -0300

    add & setup files at :review

---
+++ src/index.js
@@ -0,0 +1,69 @@
+/* eslint-disable no-alert */
+import genHTML from './gen_html.js';
+import addNew from './add.js';
+import './style.css';
+
+let listArr = [];
+
+const list = document.getElementById('list');
+
+const reload = () => {
+  if (localStorage.getItem('todoList')) {
+    const oldStorage = localStorage.getItem('todoList');
+    const newStorage = JSON.parse(oldStorage);
+    list.textContent = '';
+    listArr = newStorage;
+    genHTML(list, listArr);
+  } else {
+    const defaultItem = [
+      {
+        description: 'Add your first task',
+        completed: false,
+        index: 1,
+      },
+    ];
+    const newStorage = JSON.stringify(defaultItem);
+    listArr = defaultItem;
+    localStorage.setItem('todoList', newStorage);
+    list.innerHTML = '';
+    genHTML(list, JSON.parse(newStorage));
+  }
+  const checkboxes = document.querySelectorAll('.checkbox');
+  for (let i = 0; i < checkboxes.length; i += 1) {
+    checkboxes[i].addEventListener('change', () => {
+      reload();
+    });
+  }
+};
+
+window.onload = reload();
+
+const task = document.getElementById('add-item');
+const addNewBtn = document.getElementById('add-new-btn');
+addNewBtn.addEventListener('click', () => {
+  if (task.value.length > 0) {
+    listArr.push(addNew(task.value, listArr.length + 1));
+    const newStorage = JSON.stringify(listArr);
+    localStorage.setItem('todoList', newStorage);
+    task.value = '';
+    reload();
+  } else {
+    alert('Tasks must contain text.\nTry typing something into the "Add to your list..." input.');
+  }
+});
+
+const clrBtn = document.getElementById('clear-all');
+clrBtn.addEventListener('click', () => {
+  listArr = listArr.filter((element) => element.completed === false);
+  listArr.forEach((element, index) => {
+    element.index = index + 1;
+  });
+
+  localStorage.setItem('todoList', JSON.stringify(listArr));
+  reload();
+});
+
+const checkBtn = document.getElementById('check-all');
+checkBtn.addEventListener('click', () => {
+
+});
